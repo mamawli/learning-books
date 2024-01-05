47fc2679fc1ec01c7c2a9da6ac1f2fefdfc39dff
mjavadnasr <m.javad.nasrolla@gmail.com>
2024-01-05 16:14:11 +0330

    handle exception sample

--- Practical-Unit_testing/src/test/java/MockitoTest.java
+++ Practical-Unit_testing/src/test/java/MockitoTest.java
@@ -7,23 +7,30 @@ import static org.mockito.Mockito.when;
 
 public class MockitoTest {
 
+    private final Car car = Mockito.mock(Car.class);
+
     @Test
     public void testWithoutUsingMockito() {
-        Car car = Mockito.mock(Car.class);
         assertFalse("The default boolean method should return false at the first without using Mockito",
                 car.needFuel());
         assertEquals("The default double method should return 0.0",
-                0.0,car.getEngineTemperature(), 1);
+                0.0, car.getEngineTemperature(), 1);
     }
 
     @Test
-    public void testWithUsingMockito(){
-        Car car = Mockito.mock(Car.class);
+    public void testWithUsingMockito() {
         assertFalse("The default boolean method should return false at the first without using Mockito",
                 car.needFuel());
         when(car.needFuel()).thenReturn(true);
 
         assertTrue("now the value of need fuel method is true",
                 car.needFuel());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testMockitoWithException() {
+        when(car.needFuel()).thenThrow(RuntimeException.class);
+
+        car.needFuel();
     }
 }
