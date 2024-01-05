58aa70de388b4a30209a8f27b5c6f8811a355900
mjavadnasr <m.javad.nasrolla@gmail.com>
2024-01-05 15:28:59 +0330

    init practical unit testing

---
+++ Practical-Unit-Testing/src/main/java/book/javad/practicalunittesting/PracticalUnitTestingApplication.java
@@ -0,0 +1,13 @@
+package book.javad.practicalunittesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalUnitTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalUnitTestingApplication.class, args);
+	}
+
+}
